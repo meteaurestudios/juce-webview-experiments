bec113fba24cf4550ae9730226f2ccd89d0512f2
Baptiste Le Goff <meteaure.studios@gmail.com>
2025-05-02 12:45:44 +0200

    fix tabs vs spaces

--- res/js/index.js
+++ res/js/index.js
@@ -48,39 +48,39 @@ dropZone.addEventListener('drop', (e) => {
 });
 
 function downloadFile() {
-	downloadFunction(filePath, testFileId).then(result => {
-		console.log("File downloaded at: " + result);
+    downloadFunction(filePath, testFileId).then(result => {
+        console.log("File downloaded at: " + result);
 
-		const downLoadButton = document.getElementById("downloadButton");
-		downLoadButton.style.color = "#42f584";
-		downLoadButton.style.backgroundColor = "#444";
-		downLoadButton.textContent = "download successful"
+        const downLoadButton = document.getElementById("downloadButton");
+        downLoadButton.style.color = "#42f584";
+        downLoadButton.style.backgroundColor = "#444";
+        downLoadButton.textContent = "download successful"
 
-		const downloadedFilePath = document.getElementById("downloadedFilePath");
-		downloadedFilePath.textContent = result
-		downloadedFilePath.style.display = 'inline-block'
+        const downloadedFilePath = document.getElementById("downloadedFilePath");
+        downloadedFilePath.textContent = result
+        downloadedFilePath.style.display = 'inline-block'
 
-		const dragBox = document.getElementById("dragSource");
-		dragBox.style.display = 'block'
-	});
+        const dragBox = document.getElementById("dragSource");
+        dragBox.style.display = 'block'
+    });
 }
 
 function dragStart() {
-	window.__JUCE__.backend.emitEvent("dragStart",  {
-		fileId: testFileId
-	});
+    window.__JUCE__.backend.emitEvent("dragStart",  {
+        fileId: testFileId
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	const downLoadButton = document.getElementById("downloadButton");
-	downLoadButton.addEventListener("mouseup", () => {
-		downloadFile();
-	});
+    const downLoadButton = document.getElementById("downloadButton");
+    downLoadButton.addEventListener("mouseup", () => {
+        downloadFile();
+    });
 
-	const dragBox = document.getElementById("dragSource");
-	dragBox.addEventListener("mousedown", () => {
-		dragStart()
-	});
+    const dragBox = document.getElementById("dragSource");
+    dragBox.addEventListener("mousedown", () => {
+        dragStart()
+    });
 
-	dragBox.style.display = 'none'
+    dragBox.style.display = 'none'
 });
